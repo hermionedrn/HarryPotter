36372a84edcd56b808f031100a5a3847e6474450
hermione <hermionedaburon@gmail.com>
2023-03-14 08:56:07 +0100

    toutes les class

---
+++ src/main/java/org/example/Wizard.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Wizard extends Character{
+    private Pet pet;
+    private Wand wand;
+    private House house;
+    private List<Spell> knownSpells;
+    private List<Potion> potions;
+
+    public void defend() {
+
+    }
+}
