11a4029abfbeabe210466e3fa555ce5bef8b56e3
hermione <hermionedaburon@gmail.com>
2023-03-17 15:28:36 +0100

    début intro wizard

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez le prénom du sorcier : ");
+        String wizardName = scanner.nextLine();
+        Wizard wizard = new Wizard(wizardName);
+        System.out.println(wizard.getName());
+
     }
 }
--- src/main/java/org/example/Wizard.java
+++ src/main/java/org/example/Wizard.java
@@ -1,11 +1,34 @@
 package org.example;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Wizard extends Character{
     private Pet pet;
     private Wand wand;
     private House house;
     private List<Spell> knownSpells;
     private List<Potion> potions;
+
+    private String name; //nom du sorcier
+
+    public Wizard(String name, int level, String spell) {
+        this.name = name;
+    }
+    public Wizard(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void defend() {
 
@@ -14,4 +37,7 @@ public class Wizard extends Character{
     public void attack(Character character) {
         // code to attack
     }
+
+
 }
+
